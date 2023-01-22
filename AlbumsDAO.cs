54e8b82add541970dd650de08005eab9aa351ec7
Johnny To <johnny.to@outlook.com>
2023-01-21 19:19:16 -0500

    Connected SQL Database to Front End.

--- AlbumsDAO.cs
+++ AlbumsDAO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,36 @@ namespace MusicSQLDatabaseApp
 {
     internal class AlbumsDAO
     {
-        //dummy data
-        public List<Album> albums = new List<Album>();
+        string connectionString = "datasource=localhost;port=3306;username=root;password=root;database=music";
 
+        public List<Album> getAllAlbums()
+        {
+            List<Album> returnAlbums = new List<Album>();
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM ALBUMS", connection);
+
+            using (MySqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Album album = new Album
+                    {
+                        id = reader.GetInt32(0),
+                        albumTitle = reader.GetString(1),
+                        albumArtist = reader.GetString(2),
+                        releaseYear = reader.GetInt32(3),
+                        imageUrl = reader.GetString(4),
+                        albumGenre = reader.GetString(5)
+                    };
+                    returnAlbums.Add(album);
+                }
+            }
+            connection.Close();
+
+            return returnAlbums;
+        }
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -11,31 +11,10 @@ namespace MusicSQLDatabaseApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            AlbumsDAO albumsDAO = new AlbumsDAO();
-            Album a1 = new Album()
-            {
-                id = 1,
-                albumTitle = "first Album",
-                albumArtist = "first album artist",
-                releaseYear = 2023,
-                imageUrl = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png",
-                albumGenre = "indie",
-            };
-            Album a2 = new Album()
-            {
-                id = 2,
-                albumTitle = "second Album",
-                albumArtist = "second album artist",
-                releaseYear = 2023,
-                imageUrl = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png",
-                albumGenre = "indie",
-            };
-
-            albumsDAO.albums.Add(a1);
-            albumsDAO.albums.Add(a2);
+           AlbumsDAO albumsDAO = new AlbumsDAO();
 
             // connect list to the grid view control
-            albumBindingSource.DataSource = albumsDAO.albums;
+            albumBindingSource.DataSource = albumsDAO.getAllAlbums();
             
             dataGridView1.DataSource = albumBindingSource;
         }
